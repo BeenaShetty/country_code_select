0aa6ff9722bed941b185a4cc3e90842469feed60
BeenaShetty <binu.shetty@gmail.com>
2014-02-14 16:25:20 +0530

    Rails 4 changes
    ActionView::Helpers::InstanceTag 
    is transformed into module called ActiveModelInstanceTag
    
    ActionView::Helpers::ActiveModelInstanceTag

--- lib/country_code_select.rb
+++ lib/country_code_select.rb
@@ -4,5 +4,5 @@ require "country_code_select/form_helpers"
 require "country_code_select/instance_tag"
 
 ActionView::Base.send(:include, CountryCodeSelect::FormHelpers)
-ActionView::Helpers::InstanceTag.send(:include, CountryCodeSelect::InstanceTag)
+ActionView::Helpers::ActiveModelInstanceTag.send(:include, CountryCodeSelect::InstanceTag)
 ActionView::Helpers::FormBuilder.send(:include, CountryCodeSelect::FormBuilder)
